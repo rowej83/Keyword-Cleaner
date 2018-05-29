8db166bcd6b0e9cfa52b79e64c87ccf3ab4e2384
Jason Rowe <rowej83@gmail.com>
2018-05-29 13:03:05 -0400

    Init

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+ELEMENT.locale(ELEMENT.lang.en);
+
+function selectElementContents(el) {
+    var body = document.body, range, sel;
+    if (document.createRange && window.getSelection) {
+        range = document.createRange();
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        try {
+            range.selectNodeContents(el);
+            sel.addRange(range);
+        } catch (e) {
+            range.selectNode(el);
+            sel.addRange(range);
+        }
+    } else if (body.createTextRange) {
+        range = body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+}
